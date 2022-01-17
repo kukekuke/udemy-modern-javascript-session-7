1152a30e9dcdf677c217b4f1a3ef44bf4cf93aa9
kukekuke <86033484+kukekuke@users.noreply.github.com>
2022-01-17 22:16:32 +0900

    59.カイゼン(TODOの上限設定)

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodo5個までだよー</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
